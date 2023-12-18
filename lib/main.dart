b3f76e8a0df7667586ce876567f35e807ed71220
TheTagEnd <vashudev8777@gmail.com>
2023-12-18 21:09:57 +0530

    projects grid changed

--- lib/main.dart
+++ lib/main.dart
@@ -20,7 +20,7 @@ class MyApp extends StatelessWidget {
       debugShowCheckedModeBanner: false,
       home: Scaffold(
           body: Container(
-        padding: EdgeInsets.all(20),
+        padding: const EdgeInsets.all(20),
         child: HomeScreen(),
       )),
     );
