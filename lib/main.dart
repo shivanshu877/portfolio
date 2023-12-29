01b0ce1453e32a08d63cfae3b40ade9f35ae3ebf
TheTagEnd <vashudev8777@gmail.com>
2023-12-29 16:50:50 +0530

    error changed

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portfolioo',
+      title: 'Portfolio',
       theme: ThemeData(
         brightness: Brightness.light,
       ),
