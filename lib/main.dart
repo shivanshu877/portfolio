1e16af94f64cf157ec42637bfd976ca9b087bfd1
TheTagEnd <vashudev8777@gmail.com>
2024-01-19 10:46:55 +0530

    vv

--- lib/main.dart
+++ lib/main.dart
@@ -12,7 +12,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Portfolio',
+      title: 'Portfoli0o',
       theme: ThemeData(
         brightness: Brightness.dark,
       ),
